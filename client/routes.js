7090e044f1a500b71d794f85b6b66d9714dded81
Thaddeus Metz <temetz90@gmail.com>
2020-07-02 16:16:11 -0500

    styling updated

--- client/routes.js
+++ client/routes.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Switch, Route} from 'react-router-dom'
-import {Howl, Howler} from 'howler'
-import Keyboard from 'react-simple-keyboard'
+
 import BeetMaker from './components/BeetMaker'
 
 /**
