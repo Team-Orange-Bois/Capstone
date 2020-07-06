a38b453d1e4e4a99353a13b7fee0197606224b82
Daniel Sprague <danielaaronsprague@gmail.com>
2020-07-06 15:51:25 -0500

    changed beetmaker2 to use Tone instead of Howl

--- client/components/BeetMaker2.js
+++ client/components/BeetMaker2.js
@@ -1,38 +1,58 @@
 import React from 'react'
-import {Howl} from 'howler'
+//import {Howl} from 'howler'
+import * as Tone from 'tone'
 import {Button} from 'react-bootstrap'
 
 export default function BeetMaker2() {
-  let kick = new Howl({
-    src: [
-      'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fkick.mp3?alt=media&token=fc8bddf1-7ee7-4337-9a72-89459291bc89'
-    ]
-  })
-  let snare = new Howl({
-    src: [
-      'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fsnare.mp3?alt=media&token=09b8ea4e-8b37-4c98-beb9-a2460cdf509c'
-    ]
-  })
-  let hat = new Howl({
-    src: [
-      'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2FclosedHat.mp3?alt=media&token=dfdd2ffc-317b-465a-9499-55e4ac07a6b2'
-    ]
-  })
-  let chord1 = new Howl({
-    src: [
-      'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fchord1.mp3?alt=media&token=57981212-6cec-4d07-989e-a0d53b8aa39b'
-    ]
-  })
-  let chord2 = new Howl({
-    src: [
-      'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fchord2.mp3?alt=media&token=e2dfac36-7c86-4d8f-916b-d1f49f1068c1'
-    ]
-  })
-  let chord3 = new Howl({
-    src: [
-      'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fchord3.mp3?alt=media&token=6bbb174c-b951-4eed-ae84-de4dd65535da'
-    ]
-  })
+  // let kick = new Howl({
+  //   src: [
+  //     'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fkick.mp3?alt=media&token=fc8bddf1-7ee7-4337-9a72-89459291bc89'
+  //   ]
+  // })
+  // let snare = new Howl({
+  //   src: [
+  //     'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fsnare.mp3?alt=media&token=09b8ea4e-8b37-4c98-beb9-a2460cdf509c'
+  //   ]
+  // })
+  // let hat = new Howl({
+  //   src: [
+  //     'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2FclosedHat.mp3?alt=media&token=dfdd2ffc-317b-465a-9499-55e4ac07a6b2'
+  //   ]
+  // })
+  // let chord1 = new Howl({
+  //   src: [
+  //     'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fchord1.mp3?alt=media&token=57981212-6cec-4d07-989e-a0d53b8aa39b'
+  //   ]
+  // })
+  // let chord2 = new Howl({
+  //   src: [
+  //     'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fchord2.mp3?alt=media&token=e2dfac36-7c86-4d8f-916b-d1f49f1068c1'
+  //   ]
+  // })
+  // let chord3 = new Howl({
+  //   src: [
+  //     'https://firebasestorage.googleapis.com/v0/b/siqbeets-23b66.appspot.com/o/lofi-siq-beets%2Fchord3.mp3?alt=media&token=6bbb174c-b951-4eed-ae84-de4dd65535da'
+  //   ]
+  // })
+
+  const kick = new Tone.Sampler({
+    C3: 'http://localhost:8080/siqbeets/kick.mp3'
+  }).toMaster()
+  const snare = new Tone.Sampler({
+    C3: 'http://localhost:8080/siqbeets/snare.mp3'
+  }).toMaster()
+  const hat = new Tone.Sampler({
+    C3: 'http://localhost:8080/siqbeets/closedHat.mp3'
+  }).toMaster()
+  const chord1 = new Tone.Sampler({
+    C3: 'http://localhost:8080/siqbeets/chord1.mp3'
+  }).toMaster()
+  const chord2 = new Tone.Sampler({
+    C3: 'http://localhost:8080/siqbeets/chord2.mp3'
+  }).toMaster()
+  const chord3 = new Tone.Sampler({
+    C3: 'http://localhost:8080/siqbeets/chord3.mp3'
+  }).toMaster()
 
   let keySounds = {
     q: kick,
@@ -47,7 +67,7 @@ export default function BeetMaker2() {
     if (keySounds[identifier]) {
       const button = document.getElementById(identifier)
       button.setAttribute('class', 'butts btn active-button')
-      keySounds[identifier].play()
+      keySounds[identifier].triggerAttackRelease('C3', '4n')
     }
   }
 
--- client/components/TonePractice.js
+++ client/components/TonePractice.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import * as Tone from 'tone'
+import {Howl} from 'howler'
 
 export default function TonePractice() {
   let loopBeat
   const bassSynth = new Tone.MembraneSynth().toMaster()
+  const sampler = new Tone.Sampler({
+    C3: 'http://localhost:8080/msn.mp3'
+  }).toMaster()
 
   function setup() {
     loopBeat = new Tone.Loop(song, '4n')
-    Tone.Transport.bpm.value = 80
-    Tone.Transport.bpm.rampTo(120, 10)
+    // Tone.Transport.bpm.value = 120
+    //Tone.Transport.bpm.rampTo(120, 10)
     Tone.Transport.start()
     loopBeat.start(0)
   }
 
   function song(time) {
-    console.log(time)
     bassSynth.triggerAttackRelease('c1', '8n', time)
+    sampler.triggerAttackRelease('C3', '8n', time)
   }
 
   function changeVolume(value) {
     bassSynth.volume.value = value
-    return bassSynth
+    sampler.volume.value = value
   }
 
   return (
