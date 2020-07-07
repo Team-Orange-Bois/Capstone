254655521ac5ef89b58aeb6c5705d79d197df343
Niko Sward <nicholascsward@gmail.com>
2020-07-06 19:56:17 -0500

    drum machine

---
+++ client/components/DrumMachine.js
@@ -0,0 +1,165 @@
+import React from 'react'
+import {Form} from 'react-bootstrap'
+import * as Tone from 'tone'
+
+export default function DrumMachine() {
+  let Pattern = {
+    kick: '1n',
+    snare: '2n',
+    hat: '4n'
+  }
+  let kick = new Tone.Sampler(
+    {
+      c4: './samples/lofi/kick.mp3'
+    },
+    () => console.log('loaded kick')
+  ).toMaster()
+
+  let snare = new Tone.Sampler(
+    {
+      c4: './samples/lofi/snare.mp3'
+    },
+    () => console.log('loaded snare')
+  ).toMaster()
+
+  let hat = new Tone.Sampler(
+    {
+      c4: './samples/lofi/closedHat.mp3'
+    },
+    () => console.log('loaded hat')
+  ).toMaster()
+
+  function hatFunc() {
+    hat.triggerAttackRelease('c4', '8n')
+  }
+
+  function kickFunc() {
+    kick.triggerAttackRelease('c4', '8n')
+  }
+
+  function snareFunc() {
+    snare.triggerAttackRelease('c4', '8n')
+  }
+
+  function loopDrums() {
+    Tone.Transport.cancel()
+    let loopKick = new Tone.Loop(kickFunc, Pattern.kick)
+    let loopSnare = new Tone.Loop(snareFunc, Pattern.snare)
+    let loopHat = new Tone.Loop(hatFunc, Pattern.hat)
+    Tone.Transport.start()
+    loopKick.start(0)
+    loopSnare.start(0)
+    loopHat.start(0)
+  }
+
+  function stopLoop() {
+    Tone.Transport.cancel()
+    Tone.Transport.stop()
+  }
+  function changeVolume(value) {
+    Tone.Master.volume.value = value
+    console.log(Tone.Master.volume.value)
+  }
+
+  function changeTempo(value) {
+    Tone.Transport.bpm.value = value
+  }
+
+  function changePattern(target) {
+    Pattern[target.name] = target.value
+    loopDrums()
+  }
+
+  return (
+    <div id="controlContainer">
+      <div id="controlSliders">
+        <button type="button" onClick={() => loopDrums()}>
+          Play simple drum loop
+        </button>
+        <button type="button" onClick={() => stopLoop()}>
+          Stop drum loop
+        </button>
+        <Form>
+          <Form.Group>
+            <Form.Label>Volume</Form.Label>
+            <Form.Control
+              name="Volume"
+              type="range"
+              min="-50"
+              max="5"
+              id="volume"
+              list="volTicks"
+              defaultValue="0"
+              step="0.5"
+              onChange={e => changeVolume(e.target.value)}
+            />
+            <datalist id="volTicks">
+              <option value="-50" />
+              <option value="0" />
+              <option value="5" />
+            </datalist>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Speed</Form.Label>
+            <Form.Control
+              name="BPM"
+              type="range"
+              min="60"
+              max="200"
+              id="bpm"
+              list="bpmTicks"
+              defaultValue="120"
+              step="1"
+              size="lg"
+              onChange={e => changeTempo(e.target.value)}
+            />
+            <datalist id="bpmTicks">
+              <option value="60" />
+              <option value="120" />
+              <option value="200" />
+            </datalist>
+          </Form.Group>
+        </Form>
+      </div>
+      <div id="controlOptions" onChange={e => changePattern(e.target)}>
+        <div>
+          <label>Kick Pattern:</label>
+          <select name="kick">
+            <option value="null">None</option>
+            <option selected value="1n">
+              Whole Notes
+            </option>
+            <option value="2n">Half Notes </option>
+            <option value="4n">Quarter Notes</option>
+            <option value="8n">Eighth Notes</option>
+          </select>
+        </div>
+        <div>
+          <label>Snare Pattern:</label>
+          <select name="snare">
+            <option value="null">None</option>
+            <option value="1n">Whole Notes</option>
+            <option selected value="2n">
+              Half Notes{' '}
+            </option>
+            <option value="4n">Quarter Notes</option>
+            <option value="8n">Eighth Notes</option>
+          </select>
+        </div>
+        <div>
+          <label>Hat Pattern:</label>
+          <select name="hat">
+            <option value="null">None</option>
+            <option value="1n">Whole Notes</option>
+            <option value="2n">Half Notes </option>
+            <option selected value="4n">
+              Quarter Notes
+            </option>
+            <option value="8n">Eighth Notes</option>
+            <option value="16n">Sixteenth Notes</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/components/TonePractice.js
+++ client/components/TonePractice.js
@@ -1,26 +1,30 @@
 import React from 'react'
 import * as Tone from 'tone'
+import DrumMachine from './DrumMachine'
 
 export default function TonePractice() {
   let loopBeat
   const bassSynth = new Tone.MembraneSynth().toMaster()
+  const sampler = new Tone.Sampler({
+    C3: 'http://localhost:8080/msn.mp3'
+  }).toMaster()
 
   function setup() {
     loopBeat = new Tone.Loop(song, '4n')
-    Tone.Transport.bpm.value = 80
-    Tone.Transport.bpm.rampTo(120, 10)
+    // Tone.Transport.bpm.value = 120
+    //Tone.Transport.bpm.rampTo(120, 10)
     Tone.Transport.start()
     loopBeat.start(0)
   }
 
   function song(time) {
-    console.log(time)
     bassSynth.triggerAttackRelease('c1', '8n', time)
+    sampler.triggerAttackRelease('C3', '8n', time)
   }
 
   function changeVolume(value) {
     bassSynth.volume.value = value
-    return bassSynth
+    sampler.volume.value = value
   }
 
   return (
@@ -35,6 +39,7 @@ export default function TonePractice() {
         id="volume"
         onChange={e => changeVolume(e.target.value)}
       ></input>
+      <DrumMachine />
     </div>
   )
 }
