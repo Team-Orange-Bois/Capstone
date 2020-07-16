b0493c86fb4c55312f85b606983928eccaa1ffad
Thaddeus Metz <temetz90@gmail.com>
2020-07-16 14:31:21 -0500

    button toggle working

--- client/components/BeetMaker2.js
+++ client/components/BeetMaker2.js
@@ -215,6 +215,7 @@ export function BeetMaker2(props) {
           >
             <i className="material-icons">fiber_manual_record</i>
           </Button>
+          --
           <Button onClick={() => stopLoop()} className="butts">
             <i className="material-icons">stop</i>
           </Button>
@@ -229,9 +230,13 @@ export function BeetMaker2(props) {
           >
             Clear Loop
           </Button>
-          <Button
-            onClick={() => {
-              if (playStatus) {
+          <>
+            <input
+              className="react-switch-checkbox"
+              id="react-switch-new"
+              type="checkbox"
+              onChange={() => {
+                console.log(metronomeStatus)
                 metronomeStatus = !metronomeStatus
                 if (metronomeStatus) {
                   metronome.start(0)
@@ -240,12 +245,12 @@ export function BeetMaker2(props) {
                 } else {
                   metronome.cancel().stop()
                 }
-              }
-            }}
-            className="butts"
-          >
-            Toggle Metronome
-          </Button>
+              }}
+            />
+            <label className="react-switch-label" htmlFor="react-switch-new">
+              <span className="react-switch-button" />
+            </label>
+          </>
         </div>
         <div>
           <Form>
