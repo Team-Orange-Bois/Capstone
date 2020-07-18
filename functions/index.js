3a7501682f62a4224192c882a77f73cfd854d68f
Daniel Sprague <danielaaronsprague@gmail.com>
2020-07-18 09:01:28 -0500

    daniel's first attempt at fixing deployment with sessions

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,69 @@
 const functions = require('firebase-functions')
-const path = require('path')
-const express = require('express')
-const app = express()
-const session = require('express-session')
-const router = require('./songs')
+// const admin = require('firebase-admin')
+// const serviceAccount = require('../siqbeets-23b66-firebase-adminsdk-k1srt-00366a4f37.json')
 
-// const app = require('../server')
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// static file-serving middleware
-// app.use(express.static(path.join(__dirname, '..', 'public')))
-
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET || 'i hate CORS',
-    resave: false,
-    saveUninitialized: false
-  })
-)
-
-app.use(express.json())
-
-app.use('/api/songs', router)
-
-// sends index.html
-// app.use('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', '/public/index.html'))
+// const ref = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://siqbeets-23b66.firebaseio.com'
 // })
 
-exports.songs = functions.https.onRequest(app)
+// connect()
+//   .use(connect.cookieParser())
+//   .use(
+//     connect.session({
+//       store: new FirebaseStore({
+//         database: ref.database()
+//       }),
+//       secret: 'keyboard cat'
+//     })
+//   )
+
+const {cleanPathServer} = require('../server')
+
+const cleanPath = functions.https.onRequest((request, response) => {
+  if (!request.path) {
+    request.url = `/${request.url}` // Prepend '/' to keep query params if any
+  }
+
+  return cleanPathServer(request, response)
+})
+
+module.exports = {cleanPath}
+// const functions = require('firebase-functions')
+// const path = require('path')
+// const express = require('express')
+// const app = express()
+// const session = require('express-session')
+// const router = require('./songs')
+
+// // const app = require('../server')
+
+// // // Create and Deploy Your First Cloud Functions
+// // // https://firebase.google.com/docs/functions/write-firebase-functions
+// //
+// // exports.helloWorld = functions.https.onRequest((request, response) => {
+// //   functions.logger.info("Hello logs!", {structuredData: true});
+// //   response.send("Hello from Firebase!");
+// // });
+
+// // static file-serving middleware
+// // app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET || 'i hate CORS',
+//     resave: false,
+//     saveUninitialized: false
+//   })
+// )
+
+// app.use(express.json())
+
+// app.use('/api/songs', router)
+
+// // sends index.html
+// // app.use('*', (req, res) => {
+// //   res.sendFile(path.join(__dirname, '..', '/public/index.html'))
+// // })
+
+// exports.songs = functions.https.onRequest(app)
--- server/index.js
+++ server/index.js
@@ -1,38 +1,26 @@
-const path = require('path')
 const express = require('express')
-const morgan = require('morgan')
-const compression = require('compression')
 const session = require('express-session')
-const PORT = process.env.PORT || 8080
-const app = express()
-const socketio = require('socket.io')
-module.exports = app
+const FirebaseStore = require('connect-session-firebase')(session)
+const admin = require('firebase-admin')
+const path = require('path')
+const cors = require('cors')
+const serviceAccount = require('../siqbeets-23b66-firebase-adminsdk-k1srt-00366a4f37.json')
 
-const createApp = () => {
-  // logging middleware
-  app.use(morgan('dev'))
+// const bodyParser = require('body-parser')
+// const morgan = require('morgan')
+// const compression = require('compression')
+// const socketio = require('socket.io')
+// const PORT = process.env.PORT || 8080
 
-  // body parsing middleware
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+const ref = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://siqbeets-23b66.firebaseio.com'
+})
 
-  // compression middleware
-  app.use(compression())
-
-  // session middleware with passport
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-      resave: false,
-      saveUninitialized: false
-    })
-  )
-
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
-
-  // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
+const cleanPathServer = express()
+  .use(cors({origin: true}))
+  .use(express.static(path.join(__dirname, '..', 'public')))
+  .use((req, res, next) => {
     if (path.extname(req.path).length) {
       const err = new Error('Not found')
       err.status = 404
@@ -41,21 +29,64 @@ const createApp = () => {
       next()
     }
   })
-
-  app.use('/api', require('./api'))
-
-  // sends index.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  })
-
-  // error handling endware
-  app.use((err, req, res, next) => {
+  .use(
+    session({
+      store: new FirebaseStore({
+        database: ref.database()
+      }),
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      resave: false,
+      saveUninitialized: false
+    })
+  )
+  .use('/api', require('./api'))
+  .use((err, req, res, next) => {
     console.error(err)
     console.error(err.stack)
     res.status(err.status || 500).send(err.message || 'Internal server error.')
   })
-}
+
+module.exports = {cleanPathServer}
+
+// body parsing middleware
+// app.use(cors({origin: true}))
+// .use(bodyParser.json())
+// .use(bodyParser.urlencoded({extended: true}))
+// .use("/songs")
+
+// compression middleware
+//app.use(compression())
+
+// session middleware with passport
+
+// // static file-serving middleware
+// app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// // any remaining requests with an extension (.js, .css, etc.) send 404
+// app.use((req, res, next) => {
+//   if (path.extname(req.path).length) {
+//     const err = new Error('Not found')
+//     err.status = 404
+//     next(err)
+//   } else {
+//     next()
+//   }
+// })
+
+// app.use('/api', require('./api'))
+
+// // sends index.html
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+// })
+
+// error handling endware
+// app.use((err, req, res, next) => {
+//   console.error(err)
+//   console.error(err.stack)
+//   res.status(err.status || 500).send(err.message || 'Internal server error.')
+// })
+// }
 
 // const startListening = () => {
 //   // start listening (and create a 'server' object representing our server)
@@ -68,16 +99,16 @@ const createApp = () => {
 //   require('./socket')(io)
 // }
 
-async function bootApp() {
-  await createApp()
-  // await startListening()
-}
-// This evaluates as true when this file is run directly from the command line,
-// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
-// It will evaluate false when this module is required by another module - for example,
-// if we wanted to require our app in a test spec
-if (require.main === module) {
-  bootApp()
-} else {
-  createApp()
-}
+// async function bootApp() {
+//   await createApp()
+//   // await startListening()
+// }
+// // This evaluates as true when this file is run directly from the command line,
+// // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// // It will evaluate false when this module is required by another module - for example,
+// // if we wanted to require our app in a test spec
+// if (require.main === module) {
+//   bootApp()
+// } else {
+//   createApp()
+// }
